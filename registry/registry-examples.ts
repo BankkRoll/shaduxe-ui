27f40e1db7a8e2c5f341a05de9101c49bb71fe95
Bankk <106103625+BankkRoll@users.noreply.github.com>
2024-12-13 18:50:11 -0500

    ### 6. Update Registry Examples

--- registry/registry-examples.ts
+++ registry/registry-examples.ts
@@ -32,77 +32,77 @@ export const examples: Registry = [
     type: "registry:ui",
     files: ["example/tabs-vertical-example.tsx"],
   },
-  // button examples
-  {
-    name: "button-default-example",
-    type: "registry:ui",
-    files: ["example/button-default-example.tsx"],
-  },
-  {
-    name: "button-destructive-example",
-    type: "registry:ui",
-    files: ["example/button-destructive-example.tsx"],
-  },
-  {
-    name: "button-outline-example",
-    type: "registry:ui",
-    files: ["example/button-outline-example.tsx"],
-  },
-  {
-    name: "button-secondary-example",
-    type: "registry:ui",
-    files: ["example/button-secondary-example.tsx"],
-  },
-  {
-    name: "button-ghost-example",
-    type: "registry:ui",
-    files: ["example/button-ghost-example.tsx"],
-  },
-  {
-    name: "button-link-example",
-    type: "registry:ui",
-    files: ["example/button-link-example.tsx"],
-  },
-  {
-    name: "button-expand-icon-example",
-    type: "registry:ui",
-    files: ["example/button-expand-icon-example.tsx"],
-  },
-  {
-    name: "button-ring-hover-example",
-    type: "registry:ui",
-    files: ["example/button-ring-hover-example.tsx"],
-  },
-  {
-    name: "button-ring-hover-outline-example",
-    type: "registry:ui",
-    files: ["example/button-ring-hover-outline-example.tsx"],
-  },
-  {
-    name: "button-shine-example",
-    type: "registry:ui",
-    files: ["example/button-shine-example.tsx"],
-  },
-  {
-    name: "button-gooey-right-example",
-    type: "registry:ui",
-    files: ["example/button-gooey-right-example.tsx"],
-  },
-  {
-    name: "button-gooey-left-example",
-    type: "registry:ui",
-    files: ["example/button-gooey-left-example.tsx"],
-  },
-  {
-    name: "button-link-hover-1-example",
-    type: "registry:ui",
-    files: ["example/button-link-hover-1-example.tsx"],
-  },
-  {
-    name: "button-link-hover-2-example",
-    type: "registry:ui",
-    files: ["example/button-link-hover-2-example.tsx"],
-  },
+    // button examples
+    {
+      name: "button-default-example",
+      type: "registry:ui",
+      files: ["example/button-default-example.tsx"],
+    },
+    {
+      name: "button-destructive-example",
+      type: "registry:ui",
+      files: ["example/button-destructive-example.tsx"],
+    },
+    {
+      name: "button-outline-example",
+      type: "registry:ui",
+      files: ["example/button-outline-example.tsx"],
+    },
+    {
+      name: "button-secondary-example",
+      type: "registry:ui",
+      files: ["example/button-secondary-example.tsx"],
+    },
+    {
+      name: "button-ghost-example",
+      type: "registry:ui",
+      files: ["example/button-ghost-example.tsx"],
+    },
+    {
+      name: "button-link-example",
+      type: "registry:ui",
+      files: ["example/button-link-example.tsx"],
+    },
+    {
+      name: "button-expand-icon-example",
+      type: "registry:ui",
+      files: ["example/button-expand-icon-example.tsx"],
+    },
+    {
+      name: "button-ring-hover-example",
+      type: "registry:ui",
+      files: ["example/button-ring-hover-example.tsx"],
+    },
+    {
+      name: "button-ring-hover-outline-example",
+      type: "registry:ui",
+      files: ["example/button-ring-hover-outline-example.tsx"],
+    },
+    {
+      name: "button-shine-example",
+      type: "registry:ui",
+      files: ["example/button-shine-example.tsx"],
+    },
+    {
+      name: "button-gooey-right-example",
+      type: "registry:ui",
+      files: ["example/button-gooey-right-example.tsx"],
+    },
+    {
+      name: "button-gooey-left-example",
+      type: "registry:ui",
+      files: ["example/button-gooey-left-example.tsx"],
+    },
+    {
+      name: "button-link-hover-1-example",
+      type: "registry:ui",
+      files: ["example/button-link-hover-1-example.tsx"],
+    },
+    {
+      name: "button-link-hover-2-example",
+      type: "registry:ui",
+      files: ["example/button-link-hover-2-example.tsx"],
+    },
   // Avatar examples
   {
     name: "avatar-circle-example",
