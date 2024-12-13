2ecd15de4d0a23227c8fac583b53deadd7bf26f6
Bankk <106103625+BankkRoll@users.noreply.github.com>
2024-12-13 16:19:32 -0500

    ok default i hear you

--- components/ui/pagination.tsx
+++ components/ui/pagination.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
-import { cn } from "@/lib/utils";
+
 import { ButtonProps, buttonVariants } from "@/components/ui/button";
 import {
   ChevronLeftIcon,
   ChevronRightIcon,
   DotsHorizontalIcon,
 } from "@radix-ui/react-icons";
+
+import { cn } from "@/lib/utils";
 
 const Pagination = ({ className, ...props }: React.ComponentProps<"nav">) => (
   <nav
@@ -68,7 +70,7 @@ const PaginationPrevious = ({
 }: React.ComponentProps<typeof PaginationLink>) => (
   <PaginationLink
     aria-label="Go to previous page"
-    size="default"
+    size="md"
     className={cn("gap-1 pl-2.5", className)}
     {...props}
   >
@@ -84,7 +86,7 @@ const PaginationNext = ({
 }: React.ComponentProps<typeof PaginationLink>) => (
   <PaginationLink
     aria-label="Go to next page"
-    size="default"
+    size="md"
     className={cn("gap-1 pr-2.5", className)}
     {...props}
   >
